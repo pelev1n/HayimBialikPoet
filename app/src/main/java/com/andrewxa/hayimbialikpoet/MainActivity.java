6a1eeefca13eff6b217def3d0e86270cd339e728
pelev1n <pelevin9911@gmail.com>
2018-09-22 10:32:36 +0300

    Added fonts

--- app/src/main/java/com/andrewxa/hayimbialikpoet/MainActivity.java
+++ app/src/main/java/com/andrewxa/hayimbialikpoet/MainActivity.java
@@ -1,13 +1,25 @@
 package com.andrewxa.hayimbialikpoet;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView name;
+    TextView years;
+    Typeface myFont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = (TextView) findViewById(R.id.name);
+        years = (TextView) findViewById(R.id.years);
+
+        myFont = Typeface.createFromAsset(this.getAssets(),"fonts/shmulikclm.ttf");
+        name.setTypeface(myFont);
+        years.setTypeface(myFont);
     }
 }
