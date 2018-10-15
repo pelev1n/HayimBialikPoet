9ac31d4e7d8c66b0ed5a56077481925808f7fa95
pelev1n <pelevin9911@gmail.com>
2018-10-15 19:39:29 +0300

    Refactored card item

--- app/src/main/java/com/andrewxa/hayimbialikpoet/MainActivity.java
+++ app/src/main/java/com/andrewxa/hayimbialikpoet/MainActivity.java
@@ -1,12 +1,15 @@
 package com.andrewxa.hayimbialikpoet;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.View;
 
 import com.andrewxa.hayimbialikpoet.model.poetrycard.PoetryCard;
@@ -46,14 +49,14 @@ public class MainActivity extends AppCompatActivity{
         myrv.setLayoutManager(new GridLayoutManager(this,2));
         myrv.setAdapter(myAdapter);*/
 
+        Util.setFont(this,findViewById(R.id.name));
+        Util.setFont(this,findViewById(R.id.years));
 
         RecyclerView myrv = (RecyclerView) findViewById(R.id.recyclerview_id);
         PoetryCardAdapter myAdapter = new PoetryCardAdapter(lstPoetryCard);
-        myrv.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL));
-        myrv.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
-        myrv.setHasFixedSize(false);
-        myrv.setLayoutManager(new GridLayoutManager(MainActivity.this,2));
-       // myrv.setLayoutManager(new LinearLayoutManager(this));
+/*        myrv.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL));
+        myrv.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));*/
+        myrv.setLayoutManager(new GridLayoutManager(this, 2));
         myrv.setAdapter(myAdapter);
 
     }
@@ -77,6 +80,49 @@ public class MainActivity extends AppCompatActivity{
         realm.beginTransaction();
         realm.deleteAll();
         realm.commitTransaction();
+    }
+
+    public class GridSpacingItemDecoration extends RecyclerView.ItemDecoration {
+
+        private int spanCount;
+        private int spacing;
+        private boolean includeEdge;
+
+        public GridSpacingItemDecoration(int spanCount, int spacing, boolean includeEdge) {
+            this.spanCount = spanCount;
+            this.spacing = spacing;
+            this.includeEdge = includeEdge;
+        }
+
+        @Override
+        public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+            int position = parent.getChildAdapterPosition(view); // item position
+            int column = position % spanCount; // item column
+
+            if (includeEdge) {
+                outRect.left = spacing - column * spacing / spanCount; // spacing - column * ((1f / spanCount) * spacing)
+                outRect.right = (column + 1) * spacing / spanCount; // (column + 1) * ((1f / spanCount) * spacing)
+
+                if (position < spanCount) { // top edge
+                    outRect.top = spacing;
+                }
+                outRect.bottom = spacing; // item bottom
+            } else {
+                outRect.left = column * spacing / spanCount; // column * ((1f / spanCount) * spacing)
+                outRect.right = spacing - (column + 1) * spacing / spanCount; // spacing - (column + 1) * ((1f /    spanCount) * spacing)
+                if (position >= spanCount) {
+                    outRect.top = spacing; // item top
+                }
+            }
+        }
+    }
+
+    /**
+     * Converting dp to pixel
+     */
+    private int dpToPx(int dp) {
+        Resources r = getResources();
+        return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics()));
     }
 
 }
